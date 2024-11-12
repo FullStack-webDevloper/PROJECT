b86801bb4c19373239b19361ab1ac0c866ea2406
Altamash Ahmad <143337669+FullStack-webDevloper@users.noreply.github.com>
2024-11-12 22:06:48 +0530

    Update listing.js

--- controllers/listing.js
+++ controllers/listing.js
@@ -1,7 +1,7 @@
 const Listing= require("../models/listing");
-const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
-const mapToken=process.env.MAP_TOKEN;
-const geocodingClient =mbxGeocoding({accessToken:mapToken});
+// const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+// const mapToken=process.env.MAP_TOKEN;
+// const geocodingClient =mbxGeocoding({accessToken:mapToken});
 
 
 module.exports.index=async(req, res)=>{
@@ -26,11 +26,11 @@ module.exports.index=async(req, res)=>{
     };
 
     module.exports.createListing=async (req , res, next)=>{
-        let response=await geocodingClient.forwardGeocode({
-            query: req.body.listing.location,
-            limit: 1
-          })
-            .send();
+        // let response=await geocodingClient.forwardGeocode({
+        //     query: req.body.listing.location,
+        //     limit: 1
+        //   })
+        //     .send();
             
 
             
@@ -40,7 +40,7 @@ module.exports.index=async(req, res)=>{
         newListing.owner= req.user._id;
         newListing.image={url,filename};
 
-        newListing.geometry=response.body.features[0].geometry;
+        // newListing.geometry=response.body.features[0].geometry;
         
        let savedListing= await newListing.save();
        console.log(savedListing); 
